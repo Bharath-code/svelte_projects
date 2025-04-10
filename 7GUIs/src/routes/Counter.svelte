9c728cc30262c5754d4750b6c0985f1bd8e855ab
Bharath-code <kumarbharath63@gmail.com>
2025-04-10 17:17:25 +0530

    counter app

---
+++ 7GUIs/src/routes/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+	let count = $state(0);
+</script>
+
+<div class="flex-gap">
+	<input type="number" disabled value={count} />
+	<button onclick={() => count++}>Count</button>
+</div>
+
+<style>
+</style>
